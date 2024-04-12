d305b76fa0478032f6093beb5c518832288bb39c
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-04-12 11:55:13 +0000

    0 leaks case

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 21:06:14 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/04/04 14:44:13 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/04/12 11:52:59 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (del && lst && lst->content)
+	if (del && lst)
 	{
 		free(lst);
 	}
